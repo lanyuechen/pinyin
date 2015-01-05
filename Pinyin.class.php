4b32697409cb7d10d25839033821244a4703e067
zhenglipeng <zhenglipeng@mrocker.com>
2015-01-05 17:45:59 +0800

    添加汉语拼音区位码表

--- Pinyin.class.php
+++ Pinyin.class.php
@@ -4,12 +4,16 @@ class Pinyin {
 
 	var $map_1st;
 	var $map_2nd;
+	var $code;
+	var $py;
 
 	function __construct() {
 		$tmp = file_get_contents('./pinyin-1st.json');
-		$this->map_1st = json_decode($tmp);
+		$this->map_1st = json_decode($tmp, 'array');
+		$this->code = array_values($this->map_1st);
+		$this->py = array_keys($this->map_1st);
 		$tmp = file_get_contents('./pinyin-2nd.json');
-		$this->map_2nd = json_decode($tmp);
+		$this->map_2nd = json_decode($tmp, 'array');
 	}
 
 	public function get($str) {
@@ -22,12 +26,27 @@ class Pinyin {
 		return iconv('UTF-8', 'GB2312//IGNORE', $str);
 	}
 
-	private function code_to_py($code){
-		$map = $this->map_1st;
-		foreach ($map as $k => $v) {
-			if($code <= $v){
-				return $k;
+	//二分查找,第一级汉字
+	private function search($code, $min, $max){
+		if($max - $min == 1){
+			if($code > $this->code[$min]){
+				return $this->py[$max];
+			} else {
+				return $this->py[$min];
 			}
+		} else {
+			$mid = intval(($min + $max) / 2);
+			if($code > $this->code[$mid]){
+				return $this->search($code, $mid, $max);
+			} else {
+				return $this->search($code, $min, $mid);
+			}
+		}
+	}
+
+	private function code_to_py($code){
+		if($code < 5590){
+			return $this->search($code, 0, count($this->code) - 1);
 		}
 		$mapEx = $this->map_2nd;
 		foreach ($mapEx as $k => $v) {
